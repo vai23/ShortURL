5a3987695793863550058dbb5ae582e755c2aea1
mvaibhav <mvaibhav@vmware.com>
2021-09-11 13:53:10 +0530

    bootstraped using spring init

---
+++ shorturl/src/main/java/com/vm/shorturl/ShortUrlAppApplication.java
@@ -0,0 +1,13 @@
+package com.vm.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortUrlAppApplication.class, args);
+	}
+
+}
---
+++ shorturl/src/test/java/com/vm/shorturl/ShortUrlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
